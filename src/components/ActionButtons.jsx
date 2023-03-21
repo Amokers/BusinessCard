1dff9170150d7a7061a5d36a7abee62460f88ce4
Amokers <nicolas.bretecher@gmail.com>
2023-03-21 12:38:31 +0100

    Add space fontawesome

--- src/components/ActionButtons.jsx
+++ src/components/ActionButtons.jsx
@@ -5,11 +5,11 @@ function ActionButtons() {
     return (
         <div className="action-panel">
             <a href="mailto:nicolas.bretecher@gmail.com" className='btn btn-mail' target="_blank">
-            <i className="fa-solid fa-envelope"></i>
+            <i className="fa-solid fa-envelope fa-fw"></i>
                 Email
             </a>
             <a href="https://fr.linkedin.com/in/nicolasbretecher" className='btn btn-linkedin' target="_blank">
-                <i className="fa-brands fa-linkedin"></i>
+                <i className="fa-brands fa-linkedin fa-fw"></i>
                 Linkedin
             </a>
         </div>
